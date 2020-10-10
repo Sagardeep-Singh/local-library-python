b41b8588dfb96d0bd6f013a7bcfa98025ae692b7
Sagardeep Singh <sagardeep.0019@gmail.com>
2020-10-09 19:46:14 -0700

    commented staticfiles_storage

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -154,4 +154,4 @@ if(os.environ.get("ENV") == 'HEROKU'):
 
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
